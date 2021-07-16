f2d3788cf3b8a0fde44c00fef0136bc1fe04fae0
danilo.romano <danilo.romano@azi.com.br>
2021-07-16 12:46:45 -0400

    subindo o projeto para o git

---
+++ spec/exemplo/exemploSpec.js
@@ -0,0 +1,9 @@
+describe("Suite de teste de exemplo com NodeJS",function(){
+    it("Deve demonstrar o uso do jasmine com node",function(){
+        expect(true).toBeDefined();
+    });
+
+    it("Deve demonstrar o uso do karma em modo de escuta",function(){
+        expect(true).toEqual(true);
+    })
+})
